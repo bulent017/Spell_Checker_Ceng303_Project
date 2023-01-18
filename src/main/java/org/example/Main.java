505e30b7f0eb18db357daa33cdbd701c03e3a0cc
Furkan Rıza Parlak <furkanrizap@gmail.com>
2023-01-18 18:32:12 +0300

    Update Main.java
    Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+import structures.SpellChecker;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // This free software tool hleps you automataicly proudce misspelled text from teh corretc one.
+        
+        // Check whether the program contains resource file for the dictionary
+        if ( args[0] == null ) {
+            System.out.println("Please enter the dictionary file!");
+            System.exit(0);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        SpellChecker spellChecker = new SpellChecker(args[0]);
     }
 }

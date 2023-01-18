c2984b9bed9720178f1e33e3488f075935eb0d64
İbrahim Gülmez <72990357+Lian-La-Fey@users.noreply.github.com>
2023-01-18 18:35:58 +0300

    The project has been made user interactive.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 import structures.SpellChecker;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,5 +16,27 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         SpellChecker spellChecker = new SpellChecker(args[0]);
+        
+        // The program
+        System.out.print("Enter a text to check misspellings (To exit type '*'): ");
+        String line = scanner.nextLine();
+        
+        while ( !line.equals("*") ) {
+            line = line.replaceAll("[^A-Za-z ]", "").toLowerCase();
+            String[] words = line.split("\\s");
+
+            for (String word : words) {
+                if (spellChecker.isMisspelled(word)) {
+                    System.out.printf("The word '%s' is misspelled!\n", word);  
+                    System.out.println("\nSuggestion         Difference");
+                    List<Suggestion> suggestions = spellChecker.getSuggestions(word);
+                    suggestions.forEach(System.out::print);
+                    System.out.printf("\nDid you mean '%s'?\n\n", suggestions.get(0).word);
+                } else
+                    System.out.printf("The word '%s' is spelled correctly!\n", word);
+            }
+
+            System.out.print("\nEnter a text to check misspellings (To exit type '*'): ");
+            line = scanner.nextLine();
     }
 }
